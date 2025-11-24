b70e8aac0ebd66755edbc97ffb893d381ecda4a0
newNASA <azizbekgulomovweb@gmail.com>
2025-11-24 11:27:25 +0500

    change

--- src/components/userdata/userdata.jsx
+++ src/components/userdata/userdata.jsx
@@ -4,10 +4,10 @@ import "./userdata.scss"
 const UserData = ({ userdata }) => {
     return (
         <div className="userdata">
-            <img src={userdata.img} alt="" className="profile"/>
+            <img src={userdata.img ?? userdata.photo_url} alt="" className="profile"/>
             <h2>{userdata.first_name} {userdata.last_name}</h2>
             <p className="username">@{userdata.username}</p>
-            <p className="description">{userdata.description}</p>
+            <p className="description">{userdata.description ?? userdata.bio}</p>
         </div>
     );
 };
